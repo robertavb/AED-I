abb176bc4f65adeae6990b43f0b9f6ed9f5cee66
robertavb <robertav@usp.br>
2020-10-08 22:49:24 -0300

    Versão final do EP1

--- filaDePrioridade.c
+++ filaDePrioridade.c
@@ -1,11 +1,11 @@
 /*********************************************************************/
 /**   ACH2023 - Algoritmos e Estruturas de Dados I                  **/
 /**   EACH-USP - Segundo Semestre de 2020                           **/
-/**   <turma> - Prof. Luciano Antonio Digiampietri                  **/
+/**   <2020204> - Prof. Luciano Antonio Digiampietri                **/
 /**                                                                 **/
 /**   EP1 - Fila de Prioridade                                      **/
 /**                                                                 **/
-/**   <Roberta Vitoria Borges>                   <numero USP>          **/
+/**   <Roberta Vitoria Borges>                   <11344811>         **/
 /**                                                                 **/
 /*********************************************************************/
 
@@ -52,6 +52,7 @@ int tamanho(PFILA f){
   int tam = 0;
     PONT elementos = f->fila;
     elementos = elementos->prox;
+//percorre o arranjo enquanto os elementos são válidos
     while(elementos != f->fila){
       tam++;
       elementos = elementos->prox;
@@ -68,19 +69,22 @@ bool inserirElemento(PFILA f, int id, float prioridade){
 
   ELEMENTO* insereElemento;
   PONT ant;
-
   ant = f->fila;
   insereElemento = ant->prox;
 
+// verifica posicao para ordenação em ordem decrescente
   while(insereElemento != f->fila && insereElemento->prioridade > prioridade){
     ant = insereElemento;
     insereElemento = insereElemento->prox;
   }
-  
-    insereElemento = (PONT) malloc(sizeof(ELEMENTO));
 
+// aloca memoria para o elemento
+    insereElemento = (PONT) malloc(sizeof(ELEMENTO)); 
+
+// colocar o elemento no arranjo
     f->arranjo[id] = insereElemento;
 
+// acerta os ponteiros e insere a respectiva prioridade e id do elemento
     insereElemento->ant = ant;
     insereElemento->prox = ant->prox;
     ant->prox = insereElemento;
@@ -88,47 +92,113 @@ bool inserirElemento(PFILA f, int id, float prioridade){
     insereElemento->prioridade = prioridade;
     insereElemento->id = id;
 
+// true
   return !resposta;
+}
+
+bool verificaPrioridade(PFILA f, PONT* ant, float novaPrioridade){
+
+  bool resposta = false;
+  PONT auxiliar = f->fila->prox;
+  *ant = f->fila;
+  f->fila->id = novaPrioridade;
+
+//enquanto a prioridade antiga for maior que a nova - para reajustar o arranjo
+    while(auxiliar->prioridade >= novaPrioridade){
+    *ant = auxiliar;
+    auxiliar = auxiliar->prox;
+    }
+
+//nova prioridade já é maior
+  if(auxiliar->prioridade <= novaPrioridade){
+      return !resposta;
+    }
+
 }
 
 bool aumentarPrioridade(PFILA f, int id, float novaPrioridade){
   bool resposta = false;
-  float prioridade;
-  ELEMENTO* elemento = f->fila->prox;
-  PONT ant;
-  
 
+//verificacoes de elementos validos
   if(id < 0 || id>=f->maxElementos || f->arranjo[id] == NULL){
     return resposta;
   }
 
+  ELEMENTO* auxiliar;
+  auxiliar = f->arranjo[id];
+  PONT ant, prox;
+  
+//prioridade nova nao pode ser menor do que a antiga prioridade
   if(f->arranjo[id]->prioridade >= novaPrioridade){
     return resposta;
   }
 
-  if(elemento->prioridade > novaPrioridade){
-    elemento = elemento->prox;
-  }
+  //limpa os elementos para uma nova ordenacao pela prioridade
+  auxiliar->ant->prox = auxiliar->prox;
+  auxiliar->prox->ant = auxiliar->ant;
 
-    f->arranjo[id]->prioridade = novaPrioridade;
+//coloca a nova prioridade
+    auxiliar->prioridade = novaPrioridade;
+
+    verificaPrioridade(f,&ant,novaPrioridade);
+
+    auxiliar->ant = ant;
+    auxiliar->prox = ant->prox;
+    ant->prox = auxiliar;
+    auxiliar->prox->ant = auxiliar;
 
   return !resposta;
+}
+
+bool verificaPrioridade1(PFILA f, PONT* prox, float novaPrioridade){
+//função para ordenar quando a prioridade for reduzida
+  bool resposta = false;
+  PONT auxiliar = f->fila->ant;
+  *prox = f->fila;
+  f->fila->id = novaPrioridade;
+
+    while(auxiliar->prioridade <= novaPrioridade){
+    *prox = auxiliar;
+    auxiliar = auxiliar->ant;
+    }
+
+//nova prioridade já é maior
+  if(auxiliar->prioridade <= novaPrioridade){
+      return !resposta;
+    }
+
 }
 
 bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
   bool resposta = false;
 
-  ELEMENTO* elemento = f->fila->prox;
-
-  if(id < 0 || id >= f->maxElementos || f->arranjo[id] == NULL){
+//verificacoes de elementos validos
+  if(id < 0 || id>=f->maxElementos || f->arranjo[id] == NULL){
     return resposta;
   }
 
+  ELEMENTO* auxiliar;
+  auxiliar = f->arranjo[id];
+  PONT ant, prox;
+  
+//prioridade nova nao pode ser maior do que a antiga prioridade
   if(f->arranjo[id]->prioridade <= novaPrioridade){
     return resposta;
   }
 
-    f->arranjo[id]->prioridade = novaPrioridade;
+//limpa os elementos para uma nova ordenacao pela prioridade
+  auxiliar->prox->ant = auxiliar->ant;
+  auxiliar->ant->prox = auxiliar->prox;
+
+//coloca a nova prioridade
+    auxiliar->prioridade = novaPrioridade;
+
+  verificaPrioridade1(f,&prox,novaPrioridade);
+
+    auxiliar->prox = prox;
+    auxiliar->ant = prox->ant;
+    prox->ant = auxiliar;
+    auxiliar->ant->prox = auxiliar;
 
   return !resposta;
 
@@ -137,7 +207,7 @@ bool reduzirPrioridade(PFILA f, int id, float novaPrioridade){
 PONT removerElemento(PFILA f){
   
   PONT resposta = NULL;
-  ELEMENTO* elemento = f->fila->prox;
+  ELEMENTO* elemento = f->fila->prox; // primeiro elemento
   int remover = elemento->id;
 
   if(f->fila == f->fila->ant && f->fila == f->fila->prox){
@@ -146,9 +216,11 @@ PONT removerElemento(PFILA f){
 
     f->fila->prox = elemento->prox;
     elemento->prox->ant = f->fila;
+// remove o id
     f->arranjo[remover] = resposta;
     resposta = elemento;
 
+//removido
     return resposta;
 
 }
@@ -160,6 +232,7 @@ bool consultarPrioridade(PFILA f, int id, float* resposta){
     return resp;
   }
 
+// retorna a prioridade do elemento
   *resposta = f->arranjo[id]->prioridade;
 
   return !resp;
