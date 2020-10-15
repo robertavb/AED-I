81af46b952333d95563aed7a06c8767ddff5a1ac
robertavb <robertav@usp.br>
2020-10-15 16:17:21 -0300

    Em andamento

---
+++ matriz.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct {
+  int linhas;
+  int colunas;
+  int** M;
+}MATRIZ;
+
+MATRIZ* inicializarMatriz(int linhas, int colunas){
+  if (linhas<=0 || colunas<=0) return NULL;
+  int i;
+  MATRIZ* mat = (MATRIZ*) malloc(sizeof(MATRIZ));
+  mat->M = (int**)malloc(sizeof(int*)*linhas);
+  for (i=0; i<linhas; i++){
+      mat->M[i] = (int*)malloc(sizeof(int)*colunas);
+  }
+  mat->linhas = linhas;
+  mat->colunas = colunas;
+  return mat;
+}
+
+void imprimirMatriz(MATRIZ* mat){
+  int i,j;
+  printf("Matriz %i x %i:\n", mat->linhas, mat->colunas);
+  for (i=0; i<mat->linhas; i++){
+    for (j=0; j<mat->colunas; j++) printf("%i ", mat->M[i][j]);
+    printf("\n");
+  }
+  printf("\n");
+}
+      
+MATRIZ* multiplicarMatrizes(MATRIZ* m1, MATRIZ* m2){
+  if (m1->colunas != m2->linhas) return NULL;
+  MATRIZ* res = inicializarMatriz(m1->linhas,m2->colunas);
+  int i,j,k;
+  int valor;
+
+    //a multiplicação de matrizes é, tipicamente, implementada utilizando três laços _for_ encadeado
+    // i: linha da Matriz A
+    // j: coluna da Matriz A
+    // k: linha da Matriz B
+
+    /* Multiplicação de matrizes:
+        linha 0 de A * coluna 0 de B + linha 1 de A * coluna 0 e linha 1 de B */
+
+    if(m1->linhas == m2->colunas){
+
+        for(i = 0; i < m1->linhas; i++){
+            for(j = 0; j < m1->linhas; j++){
+                for(k = 0; k < m2->colunas; k++){
+                
+                valor =(m1->M[i][0] * m2->M[0][k]) + (m1->M[i][1] * m2->M[1][k]);
+
+                /*valor = (m1->M[i][0] * m2->M[0][j]) + (m1->M[i][1] * m2->M[1][k]);*/
+
+                printf("%i ", valor);
+                    printf("\n");
+                
+                
+               
+                }
+                
+            }
+            
+        }
+
+        /*valor = (m1->M[0][0] * m2->M[0][0]) + (m1->M[0][1] * m2->M[1][0]);*/
+
+    }
+
+    res = m1;
+
+
+  /* COMPLETAR */
+
+  return res;
+}
+      
+
+int main(){
+  
+  MATRIZ* m1 = inicializarMatriz(3, 2);
+  int i,j;
+  int cont = 1;
+  for (i=0; i<m1->linhas; i++)
+    for (j=0; j<m1->colunas; j++) {
+        m1->M[i][j] = cont;
+        cont++;
+    }
+
+  imprimirMatriz(m1);
+
+  MATRIZ* m2 = inicializarMatriz(2, 3);
+  for (i=0; i<m2->linhas; i++)
+    for (j=0; j<m2->colunas; j++) {
+        m2->M[i][j] = cont;
+        cont++;
+    }
+
+  imprimirMatriz(m2); 
+
+  MATRIZ* m3 = multiplicarMatrizes(m1, m2);
+  
+  imprimirMatriz(m3);
+
+
+  char c1;
+  printf("Pressione ENTER para terminar.\n");
+  scanf("%c", &c1);
+
+  return 0;
+}
